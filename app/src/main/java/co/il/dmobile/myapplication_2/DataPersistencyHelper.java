365dce1a4f2e6d431fcef50b413a5899d2791ed8
Danielevron <daniel.evron@post.idc.ac.il>
2022-01-14 18:03:19 +0200

    daniel my car app

--- app/src/main/java/co/il/dmobile/myapplication_2/DataPersistencyHelper.java
+++ app/src/main/java/co/il/dmobile/myapplication_2/DataPersistencyHelper.java
@@ -15,36 +15,37 @@ public class DataPersistencyHelper {
 
     public static Context Context;
 
-    public static void StoreData(List<User> users)
+    public static void StoreData(List<Car> cars)
     {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(Context);
         SharedPreferences.Editor editor = sp.edit();
-        String json = new Gson().toJson(users );
-        editor.putString("users", json);
+        String json = new Gson().toJson(cars );
+        editor.putString("cars", json);
         editor.commit();
     }
 
-    public static List<User> LoadData()
+    public static List<Car> LoadData()
     {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(Context);
-        String json = sp.getString("users",null);
+        String json = sp.getString("cars",null);
         if (json != null)
         {
-            Type type = new TypeToken<List<User>>(){}.getType();
+            Type type = new TypeToken<List<Car>>(){}.getType();
             return new Gson().fromJson(json,type);
         }
         else
         {
-            List<User> users = new ArrayList<User>();
-            users.add(new User(R.drawable.avatar1,"Contact 1","contact1@post.idc.ac.il"));
-            users.add(new User(R.drawable.avatar2,"Contact 2","contact2@post.idc.ac.il"));
-            users.add(new User(R.drawable.avatar3,"Contact 3","contact3@post.idc.ac.il"));
-            users.add(new User(R.drawable.avatar4,"Contact 4","contact4@post.idc.ac.il"));
-            users.add(new User(R.drawable.avatar5,"Contact 5","contact5@post.idc.ac.il"));
-            users.add(new User(R.drawable.avatar6,"Contact 6","contact6@post.idc.ac.il"));
-            users.add(new User(R.drawable.avatar7,"Contact 7","contact7@post.idc.ac.il"));
-            users.add(new User(R.drawable.avatar8,"Contact 8","contact8@post.idc.ac.il"));
-            return users;
+            List<Car> cars = new ArrayList<Car>();
+            cars.add(new Car(R.drawable.car01,"ford","x4","2019","200000$"));
+            cars.add(new Car(R.drawable.car02,"kia","trs4","2010","100000$"));
+            cars.add(new Car(R.drawable.car03,"bmw","i5","2020","300000$"));
+            cars.add(new Car(R.drawable.car04,"pegeout","108","2019","50000$"));
+            cars.add(new Car(R.drawable.car05,"tesla","x","2022","255000$"));
+            cars.add(new Car(R.drawable.car06,"alpha","axis","2015","108800$"));
+            cars.add(new Car(R.drawable.car07,"toyota","onix","2018","400000$"));
+            cars.add(new Car(R.drawable.car08,"chevrolet","malibu","2021","350000$"));
+
+            return cars;
         }
     }
 
